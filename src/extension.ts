9e7e3ed3f6e4624a23896f703b473198de2b92d2
Idan Rudich <idan.rudich@gmail.com>
2023-04-01 22:06:45 +0300

    WIP: done POC that it can work, needs some cleanup. Added settings, showing precentage and support for both cursor and scrolling

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,54 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+let myStatusBarItem: vscode.StatusBarItem;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "scroll-precentage" is now active!');
+export function activate({ subscriptions }: vscode.ExtensionContext) {
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('scroll-precentage.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from scroll-precentage!');
-	});
+	// register a command that is invoked when the status bar
+	// item is selected
+	const myCommandId = 'scroll-precentage.showSelectionCount';
+	subscriptions.push(vscode.commands.registerCommand(myCommandId, () => {
+		//const n = vscode.window.activeTextEditor?.visibleRanges[0].start.line;
+		//vscode.window.showInformationMessage(`Yeah, ${n} line(s) selected... Keep going!`);
+	}));
 
-	context.subscriptions.push(disposable);
+	// create a new status bar item that we can now manage
+	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	myStatusBarItem.command = myCommandId;
+	subscriptions.push(myStatusBarItem);
+
+	// register some listener that make sure the status bar 
+	// item always up-to-date
+	subscriptions.push(vscode.window.onDidChangeActiveTextEditor(getPrecentage)); // listen for chaged file/tab
+    subscriptions.push(vscode.window.onDidChangeTextEditorVisibleRanges(getPrecentage)); // listen for scroll action
+    subscriptions.push(vscode.window.onDidChangeTextEditorSelection(getPrecentage)); // listen for cursor change
+
+
+	// update status bar item once at start
+	getPrecentage();
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+function getPrecentage(): void {
+    // get config
+    const config = vscode.workspace.getConfiguration();
+    const cursorType = config.get<"cursor" | "scrollbar">('scroll-precentage.cursor', 'scrollbar');
+    if (vscode.window.activeTextEditor === undefined) {
+        // should not be possible
+        return;
+    }
+    const editor = vscode.window.activeTextEditor;
+    const maxLines = editor.document.lineCount;
+    let currentLine: number;
+
+    switch(cursorType) {
+        case "cursor":
+            currentLine = Math.max(...editor.selections.map((selection) => selection.end.line));
+            break;
+        case "scrollbar":
+            currentLine = editor.visibleRanges[0].end.line;
+            break;
+    }
+    currentLine += 1; // those lines for some resone starts counting from zero instead of 1
+    myStatusBarItem.text = `${Math.round(currentLine/maxLines*100)}%`;
+    myStatusBarItem.show();
+}
